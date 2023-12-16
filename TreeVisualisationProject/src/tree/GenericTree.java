cee2a84bb45337a229cc6b4dc471660462933019
Mingix-PT <mingixpt@gmail.com>
2023-12-16 20:37:51 +0700

    Update Class Diagram

--- TreeVisualisationProject/src/tree/GenericTree.java
+++ TreeVisualisationProject/src/tree/GenericTree.java
@@ -1,4 +1,12 @@
 package tree;
 
+import java.util.List;
+
 public class GenericTree extends Tree {
+    List<GenericTree> children;
+
+    @Override
+    protected Tree createTree() {
+        return null;
+    }
 }
--- TreeVisualisationProject/src/tree/Tree.java
+++ TreeVisualisationProject/src/tree/Tree.java
@@ -1,4 +1,12 @@
 package tree;
 
 public abstract class Tree {
+    protected abstract Tree createTree();
+    protected boolean isNull() {
+        return false;
+    }
+
+    protected void dfsTraverse () {
+
+    }
 }
