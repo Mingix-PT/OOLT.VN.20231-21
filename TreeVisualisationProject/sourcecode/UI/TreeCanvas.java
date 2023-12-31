e594f1790ecbebb170cb2f3d173c8655822e7185
npmnpm1111 <131908995+npmnpm1111@users.noreply.github.com>
2023-12-31 22:46:50 +0700

    Design node changes

--- TreeVisualisationProject/sourcecode/UI/TreeCanvas.java
+++ TreeVisualisationProject/sourcecode/UI/TreeCanvas.java
@@ -1,6 +1,7 @@
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class TreeCanvas extends Canvas {
     private GenericTree tree;
@@ -24,26 +25,44 @@ public void reset() {
         drawNode(gc, tree.root, getWidth() / 2, 50, getWidth() / 4);
     }
 
-    private void drawNode(GraphicsContext gc, Node node, double x, double y, double horizontalSpace) {
-        if (node == null) return;
+private void drawNode(GraphicsContext gc, Node node, double x, double y, double horizontalSpace) {
+    if (node == null) return;
 
-        // Draw the node
-        gc.fillOval(x - 15, y - 15, 30, 30);
-        gc.setFill(Color.WHITE);
-        gc.fillText(Integer.toString(node.value), x - 5, y + 5);
-        gc.setFill(Color.BLACK);
+    // Kích thước của vòng tròn
+    double radius = 20;
 
-        // Calculate position and draw children
-        final double childY = y + 50;
-        int numChildren = node.children.size();
-        for (int i = 0; i < numChildren; i++) {
-            double childX = x - (horizontalSpace / 2 * (numChildren - 1)) + (horizontalSpace * i);
-            // Draw line to the child node
-            gc.strokeLine(x, y, childX, childY);
-            // Recursively draw the child node
-            drawNode(gc, node.children.get(i), childX, childY, horizontalSpace / 2);
-        }
+    // Vẽ vòng tròn với viền đậm
+    gc.setStroke(Color.BLACK);
+    gc.setLineWidth(2); // Độ đậm của viền
+    gc.strokeOval(x - radius, y - radius, 2 * radius, 2 * radius);
+
+    // Văn bản nằm ở giữa vòng tròn
+    String text = Integer.toString(node.value);
+    gc.setFill(Color.BLACK);
+    // Giả định font size là 12 để tính toán
+    gc.setFont(new Font(12));
+    double textWidth = text.length() * 6; // Ước lượng chiều rộng của văn bản
+    double textHeight = 12; // Ước lượng chiều cao của văn bản
+    gc.fillText(text, x - textWidth / 2, y + textHeight / 4);
+
+    // Tính toán vị trí và vẽ các nút con
+    final double childY = y + 80; // Tăng khoảng cách giữa các nút
+    int numChildren = node.children.size();
+    for (int i = 0; i < numChildren; i++) {
+        double childX = x - (horizontalSpace / 2 * (numChildren - 1)) + (horizontalSpace * i);
+
+        // Tính tọa độ kết thúc của đường nối sao cho nó chỉ đến viền của đường tròn
+        double lineEndX = childX;
+        double lineEndY = childY - radius; // Điều chỉnh để đường nối chỉ đến viền trên cùng của đường tròn dưới
+
+        // Vẽ đường nối đến viền của nút con
+        gc.strokeLine(x, y + radius, lineEndX, lineEndY);
+
+        // Vẽ đệ quy nút con
+        drawNode(gc, node.children.get(i), childX, childY, horizontalSpace / 2);
     }
+}
+
 
     // Call this method to redraw the tree if it's been modified
     public void refresh() {
